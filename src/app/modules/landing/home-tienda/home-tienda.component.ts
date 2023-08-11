386ca31cfb506a4520a89286a1ce2eb9c8ac64ca
CarlosI01 <91437492+CarlosI01@users.noreply.github.com>
2023-08-10 21:06:15 -0500

    Catalogo,  Nosotros, Header
    xxxx

--- src/app/modules/landing/home-tienda/home-tienda.component.ts
+++ src/app/modules/landing/home-tienda/home-tienda.component.ts
@@ -2,20 +2,45 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
-
+import { MatInputModule } from '@angular/material/input';
+import { BehaviorSubject, combineLatest, Subject, takeUntil } from 'rxjs';
+import { FuseCardComponent } from '@fuse/components/card';
 @Component({
     selector     : 'home-tienda',
     templateUrl  : './home-tienda.component.html',
+    styleUrls: ['./home-tienda.component.scss'],
     encapsulation: ViewEncapsulation.None,
     standalone   : true,
-    imports      : [MatButtonModule, RouterLink, MatIconModule],
+    imports      : [MatButtonModule, RouterLink, MatIconModule, MatInputModule,FuseCardComponent ],
+    
+
 })
 export class HomeTiendaInvitadoComponent
 {
+
+
+    filters: {
+        categorySlug$: BehaviorSubject<string>;
+        query$: BehaviorSubject<string>;
+        hideCompleted$: BehaviorSubject<boolean>;
+    } = {
+        categorySlug$ : new BehaviorSubject('all'),
+        query$        : new BehaviorSubject(''),
+        hideCompleted$: new BehaviorSubject(false),
+    };
+
     /**
      * Constructor
      */
     constructor()
     {
     }
+
+
+   
+   filterByQuery(query: string): void
+   {
+       this.filters.query$.next(query);
+   }
+
 }
