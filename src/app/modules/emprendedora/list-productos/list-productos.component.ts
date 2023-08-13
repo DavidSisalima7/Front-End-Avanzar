81091ce2b735385ab293d611e20b0cbad9ab661e
SalvadorM1S <salvador.mariscal.est@tecazuay.edu.ec>
2023-08-12 20:20:57 -0500

    Creación de la tabla lista de productos-responsable

--- src/app/modules/emprendedora/list-productos/list-productos.component.ts
+++ src/app/modules/emprendedora/list-productos/list-productos.component.ts
@@ -1,10 +1,12 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 
+
 @Component({
     selector     : 'list-productos',
     standalone   : true,
     templateUrl  : './list-productos.component.html',
     encapsulation: ViewEncapsulation.None,
+
 })
 export class ListProductosEmprendedoraComponent
 {
---
+++ src/app/services/models/productosR.ts
@@ -0,0 +1,8 @@
+
+export class Productos_R {
+    idProducto: number;
+    nombreProducto: string='';
+    precioProducto: number;
+    cantidadDisponible: number;
+    estado: boolean;
+}
