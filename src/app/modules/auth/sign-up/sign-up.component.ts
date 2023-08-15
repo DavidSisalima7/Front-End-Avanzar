e0115c7f0e46d972f3023023a785390401ef3e3e
Chimbo-K18 <mateinochimbo18@gmail.com>
2023-08-15 16:39:56 -0500

    Guardar Persona Sign Up

--- src/app/modules/auth/sign-up/sign-up.component.ts
+++ src/app/modules/auth/sign-up/sign-up.component.ts
@@ -1,9 +1,9 @@
 import { NgIf } from '@angular/common';
 import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
-import { FormsModule, NgForm, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormGroup, FormsModule, NgForm, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
@@ -24,7 +24,8 @@ import { MatOptionModule } from '@angular/material/core';
     encapsulation: ViewEncapsulation.None,
     animations   : fuseAnimations,
     standalone   : true,
-    imports      : [MatOptionModule,RouterLink, NgIf, FuseAlertComponent, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, MatCheckboxModule, MatProgressSpinnerModule],
+    imports      : [MatOptionModule,RouterLink, NgIf, FuseAlertComponent, FormsModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, 
+                    MatButtonModule, MatIconModule, MatCheckboxModule, MatProgressSpinnerModule],
 })
 export class SignUpComponent implements OnInit
 {
@@ -34,7 +35,7 @@ export class SignUpComponent implements OnInit
         type   : 'success',
         message: '',
     };
-    signUpForm: UntypedFormGroup;
+    signUpForm: FormGroup;
     showAlert: boolean = false;
 
 
@@ -71,7 +72,6 @@ export class SignUpComponent implements OnInit
                 primerapellido     : ['', Validators.required],
                 segundoapellido    : ['', Validators.required],
                 correo     : ['', [Validators.required, Validators.email]],
-                password  : ['', Validators.required],
                 direccion  : ['', Validators.required],
                 celular   : ['', Validators.required],
                 genero  : ['', Validators.required],
@@ -86,42 +86,44 @@ export class SignUpComponent implements OnInit
     // @ Public methods
     // -----------------------------------------------------------------------------------------------------
 
-    /**
-     * Sign up   
-     */
-    signUp(): void
-    {
-
-        this.persona.estado = true;
-        this.persona.descripcion = 'Persona registrada desde el formulario de registro. Bienvenido y gracias por ser parte de Avanzar';
-        this.user.enabled=true;
-        this.user.visible=true;
-        this.user.username=this.signUpForm.get('correo')?.value;
-        this.user.password=this.signUpForm.get('password')?.value;
+    signUp(): void {
+        // Obtén los valores de los FormControls del FormGroup
+        const cedula = this.signUpForm.get('cedula')?.value;
         const primerNombre = this.signUpForm.get('primernombre')?.value;
-        const primerApellido = this.signUpForm.get('primerapellido')?.value;
-        this.user.name=primerNombre + ' ' + primerApellido;
+        const segundoNombre = this.signUpForm.get('segundonombre')?.value;
+        // ... otros campos ...
     
-        this.personaService.savePersona(this.persona).subscribe(data => {
-            console.log(data);
-            this.user.persona=data;
-            const rolId = 4; // ID del rol
-            this.usuarioService.registrarUsuario(this.user, rolId)
-            .subscribe(
-                (response) => {
-                  console.log('Usuario registrado exitosamente:', response);
-                  // Realizar acciones adicionales después del registro exitoso si es necesario.
-                },
-                (error) => {
-                  console.error('Error al registrar el usuario:', error);
-                  // Manejo de errores si es necesario.
-                }
-              );
-            
-            this.signUpNgForm.resetForm();
-
-        })
-
+        // Ahora puedes usar los valores en tu lógica
+        // Por ejemplo, configura los valores en el objeto persona
+        this.persona.cedula = cedula;
+        this.persona.primer_nombre = primerNombre;
+        this.persona.segundo_nombre = segundoNombre;
+        // ... otros campos ...
+    
+        // Luego, realiza la llamada al servicio para guardar la persona y el usuario
+        // ... código para guardar persona y usuario ...
+        this.personaService.savePersona(this.persona).subscribe(
+            (response) => {
+                // Si la respuesta es correcta, muestra un mensaje de éxito
+                console.log(response);
+                this.alert = {
+                    type   : 'success',
+                    message: 'Se ha creado la persona correctamente',
+                };
+                this.showAlert = true;
+            },
+            (error) => {
+                // Si la respuesta es incorrecta, muestra un mensaje de error
+                this.alert = {
+                    type   : 'error',
+                    message: 'Ha ocurrido un error al crear la persona',
+                };
+                this.showAlert = true;
+            }
+        );
+    
+        // Restablece el formulario después de guardar los datos
+        this.signUpForm.reset();
     }
 
 
