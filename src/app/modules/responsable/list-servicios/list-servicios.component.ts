2d5c543e75e84f87a4e6cdee30877305c00f39dc
David <100893416+davicho896@users.noreply.github.com>
2023-08-22 00:35:11 -0500

    tablas
    tablas

--- src/app/modules/responsable/list-servicios/list-servicios.component.ts
+++ src/app/modules/responsable/list-servicios/list-servicios.component.ts
@@ -1,17 +1,98 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Productos } from 'app/services/models/productos';
+import { ProductosService } from 'app/services/services/producto.service';
+import { MatButtonModule } from '@angular/material/button';
+import { debounceTime, map, merge, Observable, Subject, switchMap, takeUntil } from 'rxjs';
+import { PersonaService } from 'app/services/services/persona.service';
+import { Persona } from 'app/services/models/persona';
+import { Usuario } from 'app/services/models/usuario';
+import { UserService } from 'app/core/user/user.service';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
+
+//DIALOGOS
+import { MatDialog } from '@angular/material/dialog';
+import { MailboxComposeComponent } from 'app/modules/responsable/compose/compose.component';
+
 
 @Component({
     selector     : 'list-servicios',
     standalone   : true,
     templateUrl  : './list-servicios.component.html',
     encapsulation: ViewEncapsulation.None,
+    imports: [MatFormFieldModule, MatInputModule, MatTableModule, MatSortModule, MatPaginatorModule,
+      MatIconModule, MatButtonModule, CommonModule],
 })
 export class ListRespServiciosComponent
 {
-    /**
-     * Constructor
-     */
-    constructor()
+  displayedColumns: string[] = ['idProducto', 'nombreProducto', 'precioProducto', 'cantidaDisponible', 'estado'];
+  dataSource: MatTableDataSource<Productos>;
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
+  searchInputControl: UntypedFormControl = new UntypedFormControl();
+  private _unsubscribeAll: Subject<any> = new Subject<any>();
+  isLoading: boolean = false;
+
+
+
+  /**
+   * Constructor
+   */
+  constructor(private productoService: ProductosService, private _router: Router,private _matDialog: MatDialog
+    ) {
+  }
+  ngOnInit(): void {
+    this.listarRegistros();
+
+  }
+
+
+  listarRegistros() {
+    this.productoService.listarProducto().subscribe(
+      (datos: Productos[]) => {
+        this.dataSource = new MatTableDataSource<Productos>(datos);
+      },
+      error => {
+        console.error('Ocurrió un error al obtener la lista de personas responsables:', error);
+      }
+    );
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  redirectToRegisterResponsable() {
+    this._router.navigate(['/register-productos']);
+  }
+
+
+  //ABRIR EL MODAL
+  openComposeDialog(): void
     {
+        // Open the dialog
+        const dialogRef = this._matDialog.open(MailboxComposeComponent);
+
+        dialogRef.afterClosed()
+            .subscribe((result) =>
+            {
+                console.log('Compose dialog was closed!');
+            });
     }
 }
